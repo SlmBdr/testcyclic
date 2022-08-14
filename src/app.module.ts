195176e71b9f33e73536132175443734186ba174
rakakhrl <raka.khairil@gmail.com>
2022-08-14 15:03:15 +0700

    ✨ feat(app.module.ts): create a connection to mongodb atlas cluster
    Create a connection to mongodb atlas cluster

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('CLUSTER_URI'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
